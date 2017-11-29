f7d5ca2fe34f885f6e179ef3bf1de8ab17eccf39
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2017-11-29 23:03:03 +0100

    Use more frame-friendly keys to move the player.

--- js/joystick_keyboard.js
+++ js/joystick_keyboard.js
@@ -2,28 +2,28 @@ function joystick_keyboard_keyDownHandler(tEvent)
 {
 	var fSomethingHappened = false;
 	
-	if( tEvent.charCode==0 & tEvent.keyCode==38 )
+	if( tEvent.charCode==0 & tEvent.keyCode==87 )
 	{
 		ucJoystick |= JOYSTICKBIT_Up;
 		fSomethingHappened = true;
 	}
-	else if( tEvent.charCode==0 & tEvent.keyCode==37 )
+	else if( tEvent.charCode==0 & tEvent.keyCode==65 )
 	{
 		ucJoystick |= JOYSTICKBIT_Left;
 		fSomethingHappened = true;
 	}
-	else if( tEvent.charCode==0 & tEvent.keyCode==40 )
+	else if( tEvent.charCode==0 & tEvent.keyCode==83 )
 	{
 		ucJoystick |= JOYSTICKBIT_Down;
 		fSomethingHappened = true;
 	}
-	else if( tEvent.charCode==0 & tEvent.keyCode==39 )
+	else if( tEvent.charCode==0 & tEvent.keyCode==68 )
 	{
 		ucJoystick |= JOYSTICKBIT_Right;
 		fSomethingHappened = true;
 	}
-	/* This is CTRL on the PC. */
-	else if( tEvent.charCode==0 & tEvent.keyCode==17 )
+	/* This is SHIFT on the PC. */
+	else if( tEvent.charCode==0 & tEvent.keyCode==16 )
 	{
 		ucJoystick |= JOYSTICKBIT_Fire;
 		fSomethingHappened = true;
@@ -47,28 +47,28 @@ function joystick_keyboard_keyUpHandler(tEvent)
 {
 	var fSomethingHappened = false;
 	
-	if( tEvent.charCode==0 & tEvent.keyCode==38 )
+	if( tEvent.charCode==0 & tEvent.keyCode==87 )
 	{
 		ucJoystick &= 0xff-JOYSTICKBIT_Up;
 		fSomethingHappened = true;
 	}
-	else if( tEvent.charCode==0 & tEvent.keyCode==37 )
+	else if( tEvent.charCode==0 & tEvent.keyCode==65 )
 	{
 		ucJoystick &= 0xff-JOYSTICKBIT_Left;
 		fSomethingHappened = true;
 	}
-	else if( tEvent.charCode==0 & tEvent.keyCode==40 )
+	else if( tEvent.charCode==0 & tEvent.keyCode==83 )
 	{
 		ucJoystick &= 0xff-JOYSTICKBIT_Down;
 		fSomethingHappened = true;
 	}
-	else if( tEvent.charCode==0 & tEvent.keyCode==39 )
+	else if( tEvent.charCode==0 & tEvent.keyCode==68 )
 	{
 		ucJoystick &= 0xff-JOYSTICKBIT_Right;
 		fSomethingHappened = true;
 	}
-	/* This is CTRL on the PC. */
-	else if( tEvent.charCode==0 & tEvent.keyCode==17 )
+	/* This is SHIFT on the PC. */
+	else if( tEvent.charCode==0 & tEvent.keyCode==16 )
 	{
 		ucJoystick &= 0xff-JOYSTICKBIT_Fire;
 		fSomethingHappened = true;
